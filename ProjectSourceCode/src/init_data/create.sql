47f754453c52d3f256ed6d91f67a63a5a1241b0c
Vladr235 <owen.villas@gmail.com>
2024-04-04 14:09:43 -0600

    added user table initialization

---
+++ ProjectSourceCode/src/init_data/create.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  username VARCHAR(50) PRIMARY KEY,
+  password CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups (
+  group_name VARCHAR(50) PRIMARY KEY,
+  friends CHAR(60) NOT NULL,
+  transaction CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS friends;
+CREATE TABLE friends (
+  friend VARCHAR(50) PRIMARY KEY
+);
