dad775155cd566bbe06b8f55f93a62ef13a81dd4
NoahBays <noba1295@colorado.edu>
2024-04-15 23:50:44 -0600

    Fixed issues with sql, updated main to dislay nav, and updated home page look now with balances

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,15 +2,15 @@
 -- Create the users table
 CREATE TABLE users (
     username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255)
-    wallet float
+    password VARCHAR(255),
+    wallet FLOAT
 );
 
 -- Create the friends table
 CREATE TABLE friendships (
     user_username VARCHAR(255),
     friend_username VARCHAR(255),
-    outstanding_balance float,
+    outstanding_balance FLOAT,
     PRIMARY KEY (user_username, friend_username),
     FOREIGN KEY (user_username) REFERENCES users(username),
     FOREIGN KEY (friend_username) REFERENCES users(username)
@@ -20,6 +20,7 @@ CREATE TABLE friendships (
 CREATE TABLE groups (
     id INT PRIMARY KEY,
     group_admin_username VARCHAR(255),
+    group_name VARCHAR(255),
     FOREIGN KEY (group_admin_username) REFERENCES users(username)
 );
 
@@ -41,8 +42,8 @@ CREATE TABLE transactions_group(
   requester_username VARCHAR(255),
   group_id INT,
   FOREIGN KEY (group_id) REFERENCES groups(id),
-  FOREIGN KEY (requester_username) REFERENCES users(username),
-)
+  FOREIGN KEY (requester_username) REFERENCES users(username)
+);
 CREATE TABLE transactions_individual(
   charge_amount FLOAT,
   charge_desc CHAR(50),
@@ -52,5 +53,4 @@ CREATE TABLE transactions_individual(
   group_id INT,
   FOREIGN KEY (sender_username) REFERENCES users(username),
   FOREIGN KEY (recipient_username) REFERENCES users(username)
-)
-Cr
+);
