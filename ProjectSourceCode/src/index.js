7359568ee8700b28f3899781a1e0a471958151a0
Benjamin Booker <bebo4432@colorado.edu>
2024-04-18 22:24:16 -0600

    Made it so clicking the names of users in the friends list pops up with a super basic friend info page,can be used to model group as well

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -211,27 +211,17 @@ app.get("/home", (req, res) => {
 app.get("/viewUser/:username", async (req, res) => {
   const loggedInUsername = req.session.user.username;
   const visitingUsername = req.params.username;
-
-  try {
-    const friendship = await db.oneOrNone(
-      "SELECT * FROM friendships WHERE (user_username = $1 AND friend_username = $2) OR (user_username = $2 AND friend_username = $1)",
-      [loggedInUsername, visitingUsername]
-    );
-
-    //converts to boolean
-    const isFriend = !!friendship;
-    if (!isFriend) {
-      res.redirect("/home");
-      return;
-    }
-    else {
-    // Fetch the user data of the visiting user
-    res.render("pages/viewUser", {isFriend });
-    }
-  } catch (error) {
-    console.error(error);
-    alert("An error occurred while fetching user data");
-  }
+  const friendship = await db.manyOrNone(
+    "SELECT * FROM friendships WHERE (user_username = $1 AND friend_username = $2) OR (user_username = $2 AND friend_username = $1)",
+    [loggedInUsername, visitingUsername]
+  );
+  const isFriend = !!friendship;
+  
+  // Fetch the user data of the visiting user
+  const visitingUser = await db.oneOrNone("SELECT * FROM users WHERE username = $1", [visitingUsername]);
+  const balanceRow = await db.oneOrNone("SELECT outstanding_balance FROM friendships WHERE user_username = $1 AND friend_username = $2", [loggedInUsername, visitingUsername]);
+  const outstanding_balance = balanceRow ? balanceRow.outstanding_balance : null;
+  res.render("pages/viewUser", { friend: visitingUser, balance: outstanding_balance, isFriend });
 });
 
 app.get("/group", function (req, res) {
